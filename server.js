4439a0f0d8f9854e9f168d1f3e5b247ebada66b8
sulyap <sulyap@gmail.com>
2016-07-11 09:36:23 +0000

    add google

--- server.js
+++ server.js
@@ -114,6 +114,75 @@ apiRoutes.get('/users', function(req, res) {
 // apply the routes to our application with the prefix /api
 app.use('/api', apiRoutes);
 
+/*
+ |--------------------------------------------------------------------------
+ | Login with Google
+ |--------------------------------------------------------------------------
+ */
+app.post('/auth/google', function(req, res) {
+    
+    alert("success enter google");
+  /*var accessTokenUrl = 'https://accounts.google.com/o/oauth2/token';
+  var peopleApiUrl = 'https://www.googleapis.com/plus/v1/people/me/openIdConnect';
+  var params = {
+    code: req.body.code,
+    client_id: req.body.clientId,
+    client_secret: config.GOOGLE_SECRET,
+    redirect_uri: req.body.redirectUri,
+    grant_type: 'authorization_code'
+  };
+
+  // Step 1. Exchange authorization code for access token.
+  request.post(accessTokenUrl, { json: true, form: params }, function(err, response, token) {
+    var accessToken = token.access_token;
+    var headers = { Authorization: 'Bearer ' + accessToken };
+
+    // Step 2. Retrieve profile information about the current user.
+    request.get({ url: peopleApiUrl, headers: headers, json: true }, function(err, response, profile) {
+      if (profile.error) {
+        return res.status(500).send({message: profile.error.message});
+      }
+      // Step 3a. Link user accounts.
+      if (req.header('Authorization')) {
+        User.findOne({ google: profile.sub }, function(err, existingUser) {
+          if (existingUser) {
+            return res.status(409).send({ message: 'There is already a Google account that belongs to you' });
+          }
+          var token = req.header('Authorization').split(' ')[1];
+          var payload = jwt.decode(token, config.TOKEN_SECRET);
+          User.findById(payload.sub, function(err, user) {
+            if (!user) {
+              return res.status(400).send({ message: 'User not found' });
+            }
+            user.google = profile.sub;
+            user.picture = user.picture || profile.picture.replace('sz=50', 'sz=200');
+            user.displayName = user.displayName || profile.name;
+            user.save(function() {
+              var token = createJWT(user);
+              res.send({ token: token });
+            });
+          });
+        });
+      } else {
+        // Step 3b. Create a new user account or return an existing one.
+        User.findOne({ google: profile.sub }, function(err, existingUser) {
+          if (existingUser) {
+            return res.send({ token: createJWT(existingUser) });
+          }
+          var user = new User();
+          user.google = profile.sub;
+          user.picture = profile.picture.replace('sz=50', 'sz=200');
+          user.displayName = profile.name;
+          user.save(function(err) {
+            var token = createJWT(user);
+            res.send({ token: token });
+          });
+        });
+      }
+    });
+  });*/
+});
+
 app.get('/setup', function(req, res) {
 
     var mySchema = User.schema;
@@ -133,6 +202,10 @@ app.get('/setup', function(req, res) {
             });
         });
     });
+    
+    /*google
+    209006833609-dq74fhrur8nkg56k088fcgoapenuo85i.apps.googleusercontent.com
+     hWebd7iWiLPHd1KHI6FnWyO5*/
      
     /*var testdata = new  User({
         name: "admin",
